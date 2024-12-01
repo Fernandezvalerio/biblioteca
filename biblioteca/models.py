cdef88964d5a416f0cd6e3d4452b9716a8805243
Fernandezvalerio <vfernandezla@unsa.edu.pe>
2024-12-01 18:18:04 -0500

    creacion de los modelos del proyecto

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+# modelos de la biblioteca:from django.db import models
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=100)
+    correo = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.nombre
+
+class Libro(models.Model):
+    titulo = models.CharField(max_length=200)
+    autor = models.CharField(max_length=100)
+    genero = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.titulo
+
+class Opinion(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    libro = models.ForeignKey(Libro, on_delete=models.CASCADE)
+    texto = models.TextField()
+    fecha = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Opinión de {self.usuario.nombre} sobre {self.libro.titulo}"
+
