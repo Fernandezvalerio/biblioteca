8ad7a9aa4d5dfe65d6ab116ddfe4b08458415b6d
Fernandezvalerio <vfernandezla@unsa.edu.pe>
2024-12-01 18:25:31 -0500

    registrar modelos en admin y creacion de admin en django

--- biblioteca/admin.py
+++ biblioteca/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+# registrar modelos aqui
+from django.contrib import admin
+from .models import Usuario, Libro, Opinion
+
+admin.site.register(Usuario)
+admin.site.register(Libro)
+admin.site.register(Opinion)
+
