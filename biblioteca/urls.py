0a24abd363307e7dd7bc104593a95b1cb2005516
Fernandezvalerio <vfernandezla@unsa.edu.pe>
2024-12-01 19:39:27 -0500

    inteto 4 y mejora del css y creacion de inicio.html

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('usuarios/crear/', views.crear_usuario, name='crear_usuario'),
+    path('', views.pagina_inicio, name='pagina_inicio'),
     path('usuarios/', views.lista_usuarios, name='lista_usuarios'),
-    path('libros/crear/', views.crear_libro, name='crear_libro'),
     path('libros/', views.lista_libros, name='lista_libros'),
-    path('opiniones/crear/', views.crear_opinion, name='crear_opinion'),
     path('opiniones/', views.lista_opiniones, name='lista_opiniones'),
+    path('crear_usuario/', views.crear_usuario, name='crear_usuario'),
+    path('crear_libro/', views.crear_libro, name='crear_libro'),
+    path('crear_opinion/', views.crear_opinion, name='crear_opinion'),
 ]
+
--- biblioteca/views.py
+++ biblioteca/views.py
@@ -5,6 +5,13 @@ from django.shortcuts import render, redirect
 from .forms import UsuarioForm, LibroForm, OpinionForm
 from .models import Usuario, Libro, Opinion
 
+# Función para la página inicial
+def pagina_inicio(request):
+    libros = Libro.objects.all()
+    opiniones = Opinion.objects.all()
+    return render(request, 'biblioteca/inicio.html', {'libros': libros, 'opiniones': opiniones})
+
+# Función para crear un usuario
 def crear_usuario(request):
     if request.method == 'POST':
         form = UsuarioForm(request.POST)
@@ -15,6 +22,7 @@ def crear_usuario(request):
         form = UsuarioForm()
     return render(request, 'biblioteca/crear_usuario.html', {'form': form})
 
+# Función para crear un libro
 def crear_libro(request):
     if request.method == 'POST':
         form = LibroForm(request.POST)
@@ -25,6 +33,7 @@ def crear_libro(request):
         form = LibroForm()
     return render(request, 'biblioteca/crear_libro.html', {'form': form})
 
+# Función para crear una opinión
 def crear_opinion(request):
     if request.method == 'POST':
         form = OpinionForm(request.POST)
@@ -35,15 +44,19 @@ def crear_opinion(request):
         form = OpinionForm()
     return render(request, 'biblioteca/crear_opinion.html', {'form': form})
 
+# Función para listar usuarios
 def lista_usuarios(request):
     usuarios = Usuario.objects.all()
     return render(request, 'biblioteca/lista_usuarios.html', {'usuarios': usuarios})
 
+# Función para listar libros
 def lista_libros(request):
     libros = Libro.objects.all()
     return render(request, 'biblioteca/lista_libros.html', {'libros': libros})
 
+# Función para listar opiniones
 def lista_opiniones(request):
     opiniones = Opinion.objects.all()
     return render(request, 'biblioteca/lista_opiniones.html', {'opiniones': opiniones})
+
 
